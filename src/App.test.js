efb9afcf914c7f1a4277fef4e1aa410d6bf067c7
Bailey Nguyen <baileythuong@gmail.com>
2019-10-30 14:08:08 +0700

    initial commit & functionality tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders an h1', () => {
+  const wrapper = shallow(<App />)
+  expect (wrapper.contains(<h1>Increment App</h1>)).toBe(true)
+});
+
+it("check if button is clicked", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find(".increment");
+  button.simulate("click");
+  wrapper.update();
+  expect(wrapper.find("h2").text()).toEqual("2");
 });
